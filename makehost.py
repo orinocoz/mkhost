ec3e2c963e7012e5f5ea82d111fa34b39aae4478
Stanislaw Findeisen <sfindeisen@users.noreply.github.com>
2021-02-15 19:17:43 +0100

    implement execute_cmd

--- makehost.py
+++ makehost.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 
 import logging
+import os
 import sys
+
+def execute_cmd(cmd):
+    logging.info(cmd)
+    os.system(cmd)
 
 if __name__ == "__main__":
     log_format = '[{asctime}] {levelname:8} {message}'
     logging.basicConfig(stream=sys.stderr, level=logging.DEBUG, format=log_format, style='{')
-
     logging.info("Hello from makehost!")
+    execute_cmd("ls -la")
