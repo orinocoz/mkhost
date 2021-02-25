cc806a01c91139d304df13fc62e9cd762c1ea497
Stanislaw Findeisen <sfindeisen@users.noreply.github.com>
2021-02-25 19:06:35 +0100

    implement dry-run and batch modes

--- makehost.py
+++ makehost.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import argparse
 import logging
 import os
 import sys
@@ -10,12 +11,20 @@ def execute_cmd(cmd):
     logging.info(cmd)
     os.system(cmd)
 
+# Returns the apt-get command with some default arguments applied.
+# A string.
+def apt_get_cmd(*args):
+    params = " ".join(filter(lambda x: x,
+                             ["--dry-run" if makehost_args.dry_run else None,
+                              "--yes" if makehost_args.batch else None]))
+    return "apt-get {} {}".format(params, " ".join(args))
+
 def update_pkgs():
-    execute_cmd("apt-get update")
-    execute_cmd("apt-get upgrade")
+    execute_cmd(apt_get_cmd("update"))
+    execute_cmd(apt_get_cmd("upgrade"))
 
 def install_pkg(pkgname):
-    execute_cmd("apt-get install {}".format(pkgname))
+    execute_cmd(apt_get_cmd("install", pkgname))
 
 def configure_dovecot():
     install_pkg("dovecot-imapd")
@@ -24,9 +33,34 @@ def install_postfix():
     install_pkg("postfix")
 
 if __name__ == "__main__":
-    log_format = '[{asctime}] {levelname:8} {message}'
-    logging.basicConfig(stream=sys.stderr, level=logging.DEBUG, format=log_format, style='{')
 
+    parser = argparse.ArgumentParser(
+        description='Re-configures this machine according to the configuration.',
+        add_help=True, allow_abbrev=False, epilog="""This program comes with ABSOLUTELY NO WARRANTY.""")
+
+    parser.add_argument("--batch",
+                        required=False,
+                        action="store_true",
+                        help="batch mode (non-interactive)")
+
+    parser.add_argument("--dry-run",
+                        required=False,
+                        action="store_true",
+                        help="dry run (no change)")
+
+    parser.add_argument("--verbose",
+                        required=False,
+                        action="store_true",
+                        help="verbose processing")
+
+    # Parse command line arguments
+    makehost_args = parser.parse_args()
+
+    # Setup logging
+    log_format = '[{asctime}] {levelname:8} {message}'
+    logging.basicConfig(stream=sys.stderr, level=(logging.DEBUG if makehost_args.verbose else logging.INFO), format=log_format, style='{')
+
+    # Destructively re-configure the machine
     update_pkgs()
     configure_dovecot()
     install_postfix()
