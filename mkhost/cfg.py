bd9a73b741728b58236e7a066b0a9bf168c7580d
Stanislaw Findeisen <sfindeisen@users.noreply.github.com>
2021-03-16 10:03:18 +0100

    improve the config file

--- mkhost/cfg.py
+++ mkhost/cfg.py
@@ -5,6 +5,7 @@
 ##############################################################################
 
 # A simple, 1-component hostname of the target machine.
+# This must correspond to your DNS.
 MY_HOST_NAME = "my-host"
 
 # Domain of the target machine, without the hostname.
@@ -17,10 +18,9 @@ MY_HOST_NAME = "my-host"
 # This must correspond to your DNS.
 MY_HOST_DOMAIN = "my-domain.tld"
 
-# Hosted (virtual) mail domains. MX DNS record for each of them should point
-# to MY_HOST_NAME . MY_HOST_DOMAIN . You can setup CNAME records too, if you
-# like (not required for mail).
-GUEST_DOMAINS = ["a-hosted.com", "b-hosted.org", "something-else.com"]
+# Hosted (virtual mailbox and virtual alias) mail domains. MX DNS record for each of them
+# should point to MY_HOST_NAME . MY_HOST_DOMAIN .
+HOSTED_DOMAINS = ["a-server", "b-server"]
 
 # E-mail address mapping for mail forwarding. A single address can be mapped
 # to 1 or more other addresses, possibly on multiple domains.
@@ -28,10 +28,12 @@ GUEST_DOMAINS = ["a-hosted.com", "b-hosted.org", "something-else.com"]
 # Note: you don't need to own those domains or mailboxes!
 #       You can forward anywhere.
 ADDRESS_MAP = {
-    "alice@a-hosted.com"            : "bob@something-else.com",
-    "bob@a-hosted.com"              : [ "alice@b-hosted.org", "bob@something-else.com" ],
-    "bob.lastname@b-hosted.org"     : "bob@b-hosted.org",
-    "mailtunnel@something-else.com" : "astray@gmail.com"
+    "alice@a-server"                : "postmaster@b-server",
+    "postmaster@b-server"           : ["alice@a-server", "bob@b-server"],
+    "bob@a-server"                  : "bob@b-server",
+    "mailtunnel@a-server"           : "endpoint@somewhere-else",
+    "bob@b-server"                  : "eve@c-server",
+    "postmaster@my-domain.tld"      : "postmaster@b-server"
 }
 
 # List of mail protocols to enable in Dovecot. This is mapped directly to
@@ -42,3 +44,6 @@ DOVECOT_PROTOCOLS = ["imap", "pop3"]
 # localhost (True). If all your users are local, then setting this to
 # True will improve security.
 DOVECOT_LOOPBACK_ONLY = False
+
+# Dovecot users database
+DOVECOT_USERS_DB = "/etc/dovecot/users.mkhost"
