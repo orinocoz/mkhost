e4a3c9a44deee838a0099b73483ad5926bde1e2a
Stanislaw Findeisen <sfindeisen@users.noreply.github.com>
2021-03-19 14:03:26 +0100

    improve logging (include thread id)

--- mkhost.py
+++ mkhost.py
@@ -43,7 +43,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # Setup logging
-    log_format = '[{asctime}] {levelname:8} {message}'
+    log_format = '[{asctime}] {levelname:8} {thread} {message}'
     logging.basicConfig(stream=sys.stderr, level=(logging.DEBUG if args.verbose else logging.INFO), format=log_format, style='{')
 
     # Setup global variables
