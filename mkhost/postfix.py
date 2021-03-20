c5c3f313ed6a7f1088e413b13b1dfbfbab838e29
Stanislaw Findeisen <sfindeisen@users.noreply.github.com>
2021-03-20 19:41:28 +0100

    postfix smtpd_sasl_type dovecot

--- mkhost/postfix.py
+++ mkhost/postfix.py
@@ -20,20 +20,6 @@ def postconf_set(key, value):
 def install(letsencrypt_home):
     mkhost.common.install_pkgs(["postfix"])
 
-    # The directory where local(8) UNIX-style mailboxes are kept. The default setting depends on the system type.
-    # Specify a name ending in / for maildir-style delivery.
-    #
-    # Note: maildir delivery is done with the privileges of the recipient. If you use the mail_spool_directory
-    # setting for maildir style delivery, then you must create the top-level maildir directory in advance.
-    # Postfix will not create it.
-    #
-    # http://www.postfix.org/postconf.5.html#mail_spool_directory
-    mail_spool_directory = postconf_get('mail_spool_directory') or '/var/mail/'
-    logging.debug('mail_spool_directory: {}'.format(mail_spool_directory))
-    if not mail_spool_directory.endswith('/'):
-        postconf_set('mail_spool_directory', mail_spool_directory + '/')
-    # TODO create user dirs
-
     postconf_set('biff',                         'no')
     postconf_set('broken_sasl_auth_clients',     'no')
     postconf_set('delay_warning_time',           '4h')
@@ -50,11 +36,31 @@ def install(letsencrypt_home):
 
     postconf_set('smtpd_recipient_restrictions', 'permit_mynetworks reject_unauth_destination')
     postconf_set('smtpd_relay_restrictions',     'permit_mynetworks permit_sasl_authenticated reject_unauth_destination')
-    postconf_set('smtpd_sasl_auth_enable ',      'no')
+    postconf_set('smtpd_sasl_auth_enable',       'no')
     postconf_set('smtpd_tls_auth_only',          'yes')
     postconf_set('smtpd_tls_cert_file',          mkhost.letsencrypt.cert_path(letsencrypt_home))
     postconf_set('smtpd_tls_key_file',           mkhost.letsencrypt.key_path(letsencrypt_home))
+    postconf_set('smtpd_tls_loglevel',           '1')
+    postconf_set('smtpd_tls_mandatory_ciphers',  'high')
     postconf_set('smtpd_tls_security_level',     'may')
     postconf_set('smtpd_tls_wrappermode',        'no')
 
-    # postconf_set('', '')
+    # The directory where local(8) UNIX-style mailboxes are kept. The default setting depends on the system type.
+    # Specify a name ending in / for maildir-style delivery.
+    #
+    # Note: maildir delivery is done with the privileges of the recipient. If you use the mail_spool_directory
+    # setting for maildir style delivery, then you must create the top-level maildir directory in advance.
+    # Postfix will not create it.
+    #
+    # http://www.postfix.org/postconf.5.html#mail_spool_directory
+    mail_spool_directory = postconf_get('mail_spool_directory') or '/var/mail/'
+    logging.debug('mail_spool_directory: {}'.format(mail_spool_directory))
+    if not mail_spool_directory.endswith('/'):
+        postconf_set('mail_spool_directory', mail_spool_directory + '/')
+    # TODO create user dirs
+
+    # The SASL plug-in type that the Postfix SMTP server should use for authentication.
+    # The available types are listed with the "postconf -a" command.
+    # http://www.postfix.org/postconf.5.html#smtpd_sasl_type
+    # TODO: check if dovecot is available! error if not.
+    postconf_set('smtpd_sasl_type',              'dovecot')
