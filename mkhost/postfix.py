d5f65a3383b9ef67e4c36fb4e7d83d39846cf718
Stanislaw Findeisen <sfindeisen@users.noreply.github.com>
2021-03-20 14:11:59 +0100

    implement postconf call

--- mkhost/postfix.py
+++ mkhost/postfix.py
@@ -1,5 +1,16 @@
 import mkhost.common
 
+def postconf_set(key, value):
+    mkhost.common.execute_cmd(["postconf", "-v", "-e", "{}={}".format(key,value)])
+
 # Installs and configures Postfix.
 def install():
     mkhost.common.install_pkgs(["postfix"])
+
+    postconf_set('smtp_tls_security_level',  'may')
+
+    postconf_set('smtpd_tls_security_level', 'may')
+    postconf_set('smtpd_tls_wrappermode',    'no')
+    postconf_set('smtpd_tls_auth_only',      'yes')
+
+    # postconf_set('', '')
