e4eb5d56863e3c4c8bd9b417dc95b589e1c752e7
Stanislaw Findeisen <sfindeisen@users.noreply.github.com>
2021-03-27 22:03:34 +0100

    throw if validate fails

--- mkhost/cfg_parser.py
+++ mkhost/cfg_parser.py
@@ -38,10 +38,12 @@ def get_virtual_mailboxes():
     return mailboxes
 
 def validate():
+    # check if all virtual domain mailboxes declared on the right hand side of MAIL_FORWARDING
+    # are declared in MAILBOXES
     mailboxes = get_virtual_mailboxes()
     outhosted = mkhost.common.filter_addr_in_domain(mkhost.cfg.MAILBOXES.keys(), get_outgoing_addresses())
     logging.debug("outhosted (1): {}".format(outhosted))
     outhosted = outhosted.difference(mailboxes)
     logging.debug("outhosted (2): {}".format(outhosted))
     if outhosted:
-        logging.warning("Extra addresses on the right hand side in MAIL_FORWARDING: {}. They belong to MAILBOXES domains. Did you want to declare them in MAILBOXES?".format(outhosted))
+        raise Exception("Extra addresses on the right hand side in MAIL_FORWARDING: {}. They belong to MAILBOXES domains. Did you want to declare them in MAILBOXES?".format(outhosted))
