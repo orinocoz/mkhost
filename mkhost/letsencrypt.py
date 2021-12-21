5a736f9e87440afa8e0a71e8cf4cf76c424f1292
Stanislaw Findeisen <sfindeisen@users.noreply.github.com>
2021-12-21 10:55:26 +0100

    fix certbot dry run
    Error: --dry-run does not work with run

--- mkhost/letsencrypt.py
+++ mkhost/letsencrypt.py
@@ -15,10 +15,9 @@ def key_path(letsencrypt_home):
 def install():
     mkhost.common.install_pkgs(["certbot", "python3-certbot-apache"])
     mkhost.common.execute_cmd(
-        ["certbot", "run"] +
-        (["--dry-run"] if mkhost.common.get_dry_run() else [])          +   \
-        (["-n"] if mkhost.common.get_non_interactive() else [])         +   \
+        ["certbot"] + \
+        (["certonly", "--dry-run"] if mkhost.common.get_dry_run() else ["run"]) + \
+        (["-n"] if mkhost.common.get_non_interactive() else []) + \
         ["--apache", "--redirect", "--test-cert", "-d", "{}.{}".format(mkhost.cfg.MY_HOST_NAME, mkhost.cfg.MY_HOST_DOMAIN)])
-    # TODO: improve certbot cmdline args
 
 # TODO implement renew/certonly
